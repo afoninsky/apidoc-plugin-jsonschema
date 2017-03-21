15a7f88cedee9ec23cf8f83bae594cf8bc4b4a91
Anfey <vkfont@yandex.ru>
2017-03-21 22:36:33 +0300

    .

---
+++ test/fixtures/src/doc.js
@@ -0,0 +1,10 @@
+/**
+ * @api {get} /user/:id Request User information
+ * @apiName GetUser
+ * @apiGroup User
+ * @apiDescription text
+ *
+ * @apiParam {NoGroupType} field=123 some description
+ * @apiParam (GroupName) {GroupType} some second description
+ * @apiParam {schema} test/fixtures/schemas/sample.json
+ */
